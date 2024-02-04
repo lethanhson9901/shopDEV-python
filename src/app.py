104e2b4e1ed303907e893732ee8a35d0ce0edda5
son.lethanh10 <son.lethanh10@mobifone.vn>
2024-02-04 12:27:08 +0700

    Section 4: logging like a pro

--- src/app.py
+++ src/app.py
@@ -1,57 +1,41 @@
 # src/app.py
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import time
-import logging
 from brotli_asgi import BrotliMiddleware
 from src.dbs.init_mongodb import Database, start_monitoring
 from src.utils.status_codes import STATUS_TEXTS
 import asyncio
+from src.helpers.log_config import setup_logger, log_requests, scheduled_cleanup
+
+logger = setup_logger()
+
+
 app = FastAPI()
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-logger = logging.getLogger(__name__)
-
-# Init middlewares
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+# Initialize middlewares
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 app.add_middleware(BrotliMiddleware)
 
-@app.middleware("http")
-async def log_requests(request: Request, call_next):
-    start_time = time.time()
-    
-    try:
-        response = await call_next(request)
-    except Exception as e:
-        logger.error("Request processing error: %s", str(e))
-        raise  # Re-raise the exception to handle it according to the FastAPI's error handling mechanism
-    
-    process_time = time.time() - start_time
-    client_host = request.client.host if request.client else "client host unknown"
-    request_line = f'"{request.method} {request.url.path} HTTP/{request.scope["http_version"]}" {request.url}'
-    status_text = STATUS_TEXTS.get(response.status_code, "Status Text Unknown")
-    
-    logger.info(f"{client_host} - {request_line} {response.status_code} {status_text} {process_time:.6f}")
-    
-    return response
+# Request logging middleware
+app.middleware("http")(log_requests)
+
 
 db_instance = Database()  # Instantiate the Database outside event handlers for clarity
 
 @app.on_event("startup")
 async def startup_db_client():
+    # Connect to the database
     await db_instance.connect()
+    
     # Start system resource monitoring in the background
     asyncio.create_task(start_monitoring())
+    
+    # Specify the directory where your logs are stored and start the scheduled log cleanup task
+    logs_dir = 'logs'  # Adjust this path if necessary
+    asyncio.create_task(scheduled_cleanup(logs_dir, 30))  # Schedule log cleanup task
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: str):
---
+++ src/helpers/log_config.py
@@ -0,0 +1,199 @@
+# src/helpers/log_config.py
+
+from logging.handlers import RotatingFileHandler
+import os
+import logging
+from typing import Optional
+import gzip
+import shutil
+from datetime import datetime, timedelta
+import time
+import asyncio
+from fastapi import Request, HTTPException
+from fastapi.responses import JSONResponse
+
+APP_NAME = 'python-dev'
+
+# Global flag to prevent reinitialization of logger
+logger_initialized = False
+
+
+class CustomRotatingFileHandler(RotatingFileHandler):
+    """
+    A custom file handler for rotating logs at a specified size threshold,
+    and incrementing log file names with a counter.
+    """
+    
+    def __init__(
+        self, dir_name, app_name, mode='a', max_bytes=0, 
+        backup_count=0, encoding: Optional[str] = None, delay: bool = False
+    ):
+        """
+        Initialize the handler.
+        """
+        self.dir_name = dir_name
+        self.app_name = app_name
+        super().__init__(self.get_new_logfile_name(), mode, max_bytes, backup_count, encoding, delay)
+
+    def get_new_logfile_name(self):
+        """
+        Generate a new logfile name based on the current date, existing log count,
+        and application name.
+        """
+        today = datetime.now().strftime("%Y-%m-%d")
+        log_files = [filename for filename in os.listdir(self.dir_name)
+                     if filename.startswith(self.app_name) and today in filename]
+        log_counts = [int(filename.split('-')[-1].split('.')[0]) for filename in log_files
+                      if filename.split('-')[-1].split('.')[0].isdigit()]
+        next_count = max(log_counts) + 1 if log_counts else 1
+        return os.path.join(self.dir_name, f"{self.app_name}-{today}-{next_count}.log")
+
+    def shouldRollover(self, record):
+        """
+        Determine if rollover should occur based on file size.
+        """
+        if self.maxBytes == 0:
+            return False
+        return super().shouldRollover(record)
+
+    def doRollover(self):
+        """
+        Perform a rollover, as described in __init__().
+        """
+        self.stream.close()
+        if self.backupCount > 0:
+            for i in range(self.backupCount - 1, 0, -1):
+                source = self.rotation_filename(f"{self.baseFilename}.{i}")
+                dest = self.rotation_filename(f"{self.baseFilename}.{i + 1}")
+                if os.path.exists(source):
+                    if os.path.exists(dest):
+                        os.remove(dest)
+                    os.rename(source, dest)
+            first_backup = self.rotation_filename(f"{self.baseFilename}.1")
+            if os.path.exists(first_backup):
+                os.remove(first_backup)
+            os.rename(self.baseFilename, first_backup)
+        if not self.delay:
+            self.stream = self._open()
+        self.baseFilename = self.get_new_logfile_name()
+
+
+
+class GZipRotator:
+    """
+    GZip rotator for compressing log files upon rotation.
+    """
+
+    def __call__(self, source: str, dest: str):
+        """
+        Compress the log file using gzip.
+
+        Args:
+            source (str): The source log file.
+            dest (str): The destination file path.
+        """
+        with open(source, 'rb') as f_in, gzip.open(dest + '.gz', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+        os.remove(source)
+
+
+def setup_logger() -> logging.Logger:
+    """
+    Set up and configure the logger with a custom rotating file handler and gzip rotator.
+
+    Returns:
+        logging.Logger: The configured logger.
+    """
+    global logger_initialized
+    logger = logging.getLogger(__name__)
+    if not logger_initialized:
+        logger.setLevel(logging.INFO)
+        logs_dir = 'logs'
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
+
+        handler = CustomRotatingFileHandler(
+            app_name=APP_NAME, dir_name=logs_dir, max_bytes=0,
+            backup_count=3, encoding='utf-8')
+        handler.rotator = GZipRotator()
+        handler.namer = lambda name: name.replace(".log", "") + ".gz"
+
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S')
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+        logger_initialized = True
+
+    return logger
+
+
+
+async def log_requests(request: Request, call_next):
+    """
+    Middleware function to log incoming requests and their processing time.
+
+    Args:
+        request (Request): The incoming request.
+        call_next: Function to proceed with request handling.
+
+    Returns:
+        Response: The response object to be sent back to the client.
+    """
+    logger = setup_logger()  # Ensure logger is setup correctly
+    start_time = time.time()
+
+    try:
+        response = await call_next(request)
+    except HTTPException as e:
+        logger.warning(f"HTTP exception: {e.detail} - Status: {e.status_code}")
+        return JSONResponse(status_code=e.status_code, content={"message": e.detail})
+    except Exception as e:
+        logger.error(f"Unhandled exception: {str(e)}")
+        return JSONResponse(status_code=500, content={"message": "Internal Server Error"})
+    
+    process_time = time.time() - start_time
+    client_host = request.client.host if request.client else "client host unknown"
+    request_line = f'"{request.method} {request.url.path} HTTP/{request.scope["http_version"]}"'
+    
+    logger.info(f"{client_host} - {request_line} {response.status_code} "
+                f"- Processing time: {process_time:.6f} sec")
+    return response
+
+
+def cleanup_old_logs(logs_dir='logs', days_old=30):
+    """
+    Remove log files older than a specified number of days.
+
+    Args:
+        logs_dir (str): Directory containing log files.
+        days_old (int): Age threshold in days for log files to be removed.
+    """
+    current_time = time.time()
+    for filename in os.listdir(logs_dir):
+        filepath = os.path.join(logs_dir, filename)
+        file_creation_time = os.path.getctime(filepath)
+        if current_time - file_creation_time > days_old * 86400:  # 86400 seconds in a day
+            os.remove(filepath)
+            logging.info(f"Removed old log file: {filename}")
+
+
+async def scheduled_cleanup(logs_dir='logs', days_old=30):
+    """
+    Run the cleanup task daily at 2 AM to remove old log files.
+
+    Args:
+        logs_dir (str): Directory containing log files.
+        days_old (int): Age threshold in days for log files to be removed.
+    """
+    while True:
+        now = datetime.now()
+        next_run = now.replace(hour=2, minute=0, second=0, microsecond=0) + timedelta(days=1)
+        sleep_seconds = (next_run - now).total_seconds()
+        logging.info(f"Next cleanup scheduled at {next_run.isoformat()}. "
+                     f"Sleeping for {sleep_seconds} seconds.")
+        
+        await asyncio.sleep(sleep_seconds)  # Sleep until 2 AM
+        cleanup_old_logs(logs_dir, days_old)
+        await asyncio.sleep(86400)  # Sleep for a day to wait for the next 2 AM
